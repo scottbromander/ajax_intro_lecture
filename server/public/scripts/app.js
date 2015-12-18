42a34b5a7e5421d6a5f3df1b86d589405376ed1c
Scott Bromander <scott@primeacademy.io>
2015-12-18 10:39:12 -0600

    update

--- server/public/scripts/app.js
+++ server/public/scripts/app.js
@@ -2,14 +2,18 @@ $(document).ready(function(){
     console.log("Meow! (Really would you have expected something else from me?");
 
     $(".clicky-button").on('click', clickyButton);
+
+    $(".some-button").on('click', function(){
+        console.log($(this));
+    });
 });
 
 function clickyButton(){
     $.ajax({
         type: "GET",
         url: "/kittyFoo",
-        success: function(data){
-            console.log(data);
+        success: function(gremlins){
+            console.log(gremlins);
         }
     })
 }
